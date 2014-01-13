d3a04b1af0bece13ca1dfeb95b4015fe6aecb5ea
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2014-01-13 13:21:45 -0800

    added more model classes

--- lib/presto/client/models.rb
+++ lib/presto/client/models.rb
@@ -53,58 +53,58 @@ module Presto::Client
     end
   end
 
-  #class StageStats
-  #  attr_reader :stage_id
-  #  attr_reader :state
-  #  attr_reader :done
-  #  attr_reader :nodes
-  #  attr_reader :total_splits
-  #  attr_reader :queued_splits
-  #  attr_reader :running_splits
-  #  attr_reader :completed_splits
-  #  attr_reader :user_time_millis
-  #  attr_reader :cpu_time_millis
-  #  attr_reader :wall_time_millis
-  #  attr_reader :processed_rows
-  #  attr_reader :processed_bytes
-  #  attr_reader :sub_stages
-  #
-  #  def initialize(options={})
-  #    @stage_id = options[:stage_id]
-  #    @state = options[:state]
-  #    @done = options[:done]
-  #    @nodes = options[:nodes]
-  #    @total_splits = options[:total_splits]
-  #    @queued_splits = options[:queued_splits]
-  #    @running_splits = options[:running_splits]
-  #    @completed_splits = options[:completed_splits]
-  #    @user_time_millis = options[:user_time_millis]
-  #    @cpu_time_millis = options[:cpu_time_millis]
-  #    @wall_time_millis = options[:wall_time_millis]
-  #    @processed_rows = options[:processed_rows]
-  #    @processed_bytes = options[:processed_bytes]
-  #    @sub_stages = options[:sub_stages]
-  #  end
-  #
-  #  def self.decode_hash(hash)
-  #    new(
-  #      stage_id: hash["stageId"],
-  #      state: hash["state"],
-  #      done: hash["done"],
-  #      nodes: hash["nodes"],
-  #      total_splits: hash["totalSplits"],
-  #      queued_splits: hash["queuedSplits"],
-  #      running_splits: hash["runningSplits"],
-  #      completed_splits: hash["completedSplits"],
-  #      user_time_millis: hash["userTimeMillis"],
-  #      cpu_time_millis: hash["cpuTimeMillis"],
-  #      wall_time_millis: hash["wallTimeMillis"],
-  #      processed_rows: hash["processedRows"],
-  #      processed_bytes: hash["processedBytes"],
-  #      sub_stages: hash["subStages"].map {|h| StageStats.decode_hash(h) },
-  #    )
-  #  end
-  #end
+  class StageStats
+    attr_reader :stage_id
+    attr_reader :state
+    attr_reader :done
+    attr_reader :nodes
+    attr_reader :total_splits
+    attr_reader :queued_splits
+    attr_reader :running_splits
+    attr_reader :completed_splits
+    attr_reader :user_time_millis
+    attr_reader :cpu_time_millis
+    attr_reader :wall_time_millis
+    attr_reader :processed_rows
+    attr_reader :processed_bytes
+    attr_reader :sub_stages
+
+    def initialize(options={})
+      @stage_id = options[:stage_id]
+      @state = options[:state]
+      @done = options[:done]
+      @nodes = options[:nodes]
+      @total_splits = options[:total_splits]
+      @queued_splits = options[:queued_splits]
+      @running_splits = options[:running_splits]
+      @completed_splits = options[:completed_splits]
+      @user_time_millis = options[:user_time_millis]
+      @cpu_time_millis = options[:cpu_time_millis]
+      @wall_time_millis = options[:wall_time_millis]
+      @processed_rows = options[:processed_rows]
+      @processed_bytes = options[:processed_bytes]
+      @sub_stages = options[:sub_stages]
+    end
+
+    def self.decode_hash(hash)
+      new(
+        stage_id: hash["stageId"],
+        state: hash["state"],
+        done: hash["done"],
+        nodes: hash["nodes"],
+        total_splits: hash["totalSplits"],
+        queued_splits: hash["queuedSplits"],
+        running_splits: hash["runningSplits"],
+        completed_splits: hash["completedSplits"],
+        user_time_millis: hash["userTimeMillis"],
+        cpu_time_millis: hash["cpuTimeMillis"],
+        wall_time_millis: hash["wallTimeMillis"],
+        processed_rows: hash["processedRows"],
+        processed_bytes: hash["processedBytes"],
+        sub_stages: hash["subStages"] && hash["subStages"].map {|h| StageStats.decode_hash(h) },
+      )
+    end
+  end
 
   class StatementStats
     attr_reader :state
@@ -119,22 +119,22 @@ module Presto::Client
     attr_reader :wall_time_millis
     attr_reader :processed_rows
     attr_reader :processed_bytes
-    #attr_reader :root_stage
+    attr_reader :root_stage
 
     def initialize(options={})
-      @state = state
-      @scheduled = scheduled
-      @nodes = nodes
-      @total_splits = total_splits
-      @queued_splits = queued_splits
-      @running_splits = running_splits
-      @completed_splits = completed_splits
-      @user_time_millis = user_time_millis
-      @cpu_time_millis = cpu_time_millis
-      @wall_time_millis = wall_time_millis
-      @processed_rows = processed_rows
-      @processed_bytes = processed_bytes
-      #@root_stage = root_stage
+      @state = options[:state]
+      @scheduled = options[:scheduled]
+      @nodes = options[:nodes]
+      @total_splits = options[:total_splits]
+      @queued_splits = options[:queued_splits]
+      @running_splits = options[:running_splits]
+      @completed_splits = options[:completed_splits]
+      @user_time_millis = options[:user_time_millis]
+      @cpu_time_millis = options[:cpu_time_millis]
+      @wall_time_millis = options[:wall_time_millis]
+      @processed_rows = options[:processed_rows]
+      @processed_bytes = options[:processed_bytes]
+      @root_stage = options[:root_stage]
     end
 
     def self.decode_hash(hash)
@@ -151,7 +151,79 @@ module Presto::Client
         wall_time_millis: hash["wallTimeMillis"],
         processed_rows: hash["processedRows"],
         processed_bytes: hash["processedBytes"],
-        #root_stage: StageStats.decode_hash(hash["rootStage"]),
+        root_stage: hash["rootStage"] && StageStats.decode_hash(hash["rootStage"]),
+      )
+    end
+  end
+
+  class ErrorLocation
+    attr_reader :line_number
+    attr_reader :column_number
+
+    def initialize(options={})
+      @line_number = options[:line_number]
+      @column_number = options[:column_number]
+    end
+
+    def self.decode_hash(hash)
+      new(
+        line_number: hash["lineNumber"],
+        column_number: hash["columnNumber"],
+      )
+    end
+  end
+
+  class FailureInfo
+    attr_reader :type
+    attr_reader :message
+    attr_reader :cause
+    attr_reader :suppressed
+    attr_reader :stack
+    attr_reader :error_location
+
+    def initialize(options={})
+      @type = options[:type]
+      @message = options[:message]
+      @cause = options[:cause]
+      @suppressed = options[:suppressed]
+      @stack = options[:stack]
+      @error_location = options[:error_location]
+    end
+
+    def self.decode_hash(hash)
+      new(
+        type: hash["type"],
+        message: hash["message"],
+        cause: hash["cause"],
+        suppressed: hash["suppressed"] && hash["suppressed"].map {|h| FailureInfo.decode_hash(h) },
+        stack: hash["stack"],
+        error_location: hash["errorLocation"] && ErrorLocation.decode_hash(hash["errorLocation"]),
+      )
+    end
+  end
+
+  class QueryError
+    attr_reader :message
+    attr_reader :sql_state
+    attr_reader :error_code
+    attr_reader :error_location
+    attr_reader :failure_info
+
+    def initialize(options={})
+      @message = options[:message]
+      @sql_state = options[:sql_state]
+      @error_code = options[:error_code]
+      @error_location = options[:error_location]
+      @failure_info = options[:failure_info]
+    end
+
+    def self.decode_hash(hash)
+      new(
+        message: hash["message"],
+        sql_state: hash["sqlState"],
+        error_code: hash["errorCode"],
+        error_location: hash["errorLocation"] && ErrorLocation.decode_hash(hash["errorLocation"]),
+        failure_info: hash["failureInfo"] && FailureInfo.decode_hash(hash["failureInfo"]),
       )
     end
   end
@@ -183,10 +255,10 @@ module Presto::Client
         info_uri: hash["infoUri"],
         partial_cache_uri: hash["partialCancelUri"],
         next_uri: hash["nextUri"],
-        columns: hash["columns"] ? hash["columns"].map {|h| Column.decode_hash(h) } : nil,
+        columns: hash["columns"] && hash["columns"].map {|h| Column.decode_hash(h) },
         data: hash["data"],
-        stats: StatementStats.decode_hash(hash["stats"]),
-        error: hash["error"],  # TODO
+        stats: hash["stats"] && StatementStats.decode_hash(hash["stats"]),
+        error: hash["error"] && QueryError.decode_hash(hash["error"]),
       )
     end
   end
