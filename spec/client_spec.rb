986d89c89bf3e5840b5e6ffd4b48bb67b7622000
Keith McHugh <kmcq@github.com>
2023-09-11 08:47:29 -0700

    Add specs showing that complex types are returned as arrays of values

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,15 +8,17 @@ describe Trino::Client::Client do
       [
         Models::Column.new(name: 'animal', type: 'string'),
         Models::Column.new(name: 'score', type: 'integer'),
-        Models::Column.new(name: 'name', type: 'string')
+        Models::Column.new(name: 'name', type: 'string'),
+        Models::Column.new(name: 'foods', type: 'array(string string)'),
+        Models::Column.new(name: 'traits', type: 'row(breed string, num_spots integer)')
       ]
     end
 
     it 'multiple rows' do
       rows = [
-        ['dog', 1, 'Lassie'],
-        ['horse', 5, 'Mr. Ed'],
-        ['t-rex', 37, 'Doug']
+        ['dog', 1, 'Lassie', ['kibble', 'peanut butter'], ['spaniel', 2]],
+        ['horse', 5, 'Mr. Ed', ['hay', 'sugar cubes'], ['some horse', 0]],
+        ['t-rex', 37, 'Doug', ['rodents', 'small dinos'], ['dino', 0]]
       ]
       client.stub(:run).and_return([columns, rows])
 
@@ -27,18 +29,26 @@ describe Trino::Client::Client do
       expect(rehashed[0]['animal']).to eq 'dog'
       expect(rehashed[0]['score']).to eq 1
       expect(rehashed[0]['name']).to eq 'Lassie'
+      expect(rehashed[0]['foods']).to eq ['kibble', 'peanut butter']
+      expect(rehashed[0]['traits']).to eq ['spaniel', 2]
 
       expect(rehashed[0].values[0]).to eq 'dog'
       expect(rehashed[0].values[1]).to eq 1
       expect(rehashed[0].values[2]).to eq 'Lassie'
+      expect(rehashed[0].values[3]).to eq ['kibble', 'peanut butter']
+      expect(rehashed[0].values[4]).to eq ['spaniel', 2]
 
       expect(rehashed[1]['animal']).to eq 'horse'
       expect(rehashed[1]['score']).to eq 5
       expect(rehashed[1]['name']).to eq 'Mr. Ed'
+      expect(rehashed[1]['foods']).to eq ['hay', 'sugar cubes']
+      expect(rehashed[1]['traits']).to eq ['some horse', 0]
 
       expect(rehashed[1].values[0]).to eq 'horse'
       expect(rehashed[1].values[1]).to eq 5
       expect(rehashed[1].values[2]).to eq 'Mr. Ed'
+      expect(rehashed[1].values[3]).to eq ['hay', 'sugar cubes']
+      expect(rehashed[1].values[4]).to eq ['some horse', 0]
     end
 
     it 'empty results' do
@@ -58,17 +68,21 @@ describe Trino::Client::Client do
         "animal" => "wrong",
         "score" => "count",
         "name" => nil,
+        "foods" => nil,
+        "traits" => nil
       }]
     end
 
     it 'handles too many result columns' do
-      rows = [['wrong', 'count', 'too', 'much', 'columns']]
+      rows = [['wrong', 'count', 'too', 'too', 'too', 'much', 'columns']]
       client.stub(:run).and_return([columns, rows])
 
       expect(client.run_with_names('fake query')).to eq [{
         "animal" => "wrong",
         "score" => "count",
-        "name" => 'too',
+        "name" => "too",
+        "foods" => "too",
+        "traits" => "too"
       }]
     end
   end
